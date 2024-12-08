b0e9e4acfbe4410d8563e600b21fcdd7c6cf4d59
Enrico Mischorr <enrico@mischorr.de>
2024-12-08 17:23:41 +0100

    create docs

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Klf200.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:socket, "~> 0.3"}
     ]
   end
